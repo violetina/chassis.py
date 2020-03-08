7fb6123de5183de0fa8d950598bca364c6e6bfbc
violetina <schizoid69@gmail.com>
2020-03-08 10:45:41 +0100

    temp edit

--- viaa/configuration.py
+++ viaa/configuration.py
@@ -1,8 +1,7 @@
 import os
+import sys
 import yaml
-
-
-class ConfigParser:
+class ConfigParser():
     """The ConfigParser has a config dictionary containing
     all the configuration for the Chassis, but it can also be
     used for program-specific configuration.
@@ -14,23 +13,37 @@ class ConfigParser:
     3. Config file
     4. Defaults
     """
+    path = sys.path.append(os.getcwd())
 
-    config: dict = {}
+    config = {}
 
-    def __init__(self):
+    def __init__(self, config_from_env=False):
+        self.config_from_env = config_from_env
+        self.app_cfg={'module_name':os.path.split(
+                    os.path.split(repr(__file__))[0])[-1]}
         cfg = {}
-        # TODO: Take config from commandline arguments
-        # TODO: Take config from environment variables
-        # Take config from the user's config file
-        if os.path.isfile(os.getcwd() + "/config.yml"):
-            with open(os.getcwd() + "/config.yml", "r") as ymlfile:
-                cfg: dict = yaml.load(ymlfile, Loader=yaml.FullLoader)
-        # Fallback to default
+        if self.config_from_env:
+            self.app_cfg=os.environ
+            self.config["logging"] = {"level": 10}
         else:
-            cfg = {"viaa": {"logging": {"level": 20}}}
+           # config = {}
+            
+            # TODO: Take config from commandline arguments
+            # TODO: Take config from environment variables
+            # Take config from the user's config file
+            if os.path.isfile(os.getcwd() + "/config.yml"):
+                with open(os.getcwd() + "/config.yml", "r") as ymlfile:
+                    cfg: dict = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            # Fallback to default
+            else:
+                cfg = {"viaa":{"logging": {"level": 10,"RabPub":{"host":"test","passw":"test","user":"test"}}}}
+    
+            if "viaa" in cfg:
+                self.config = cfg["viaa"]
+            if "application" in cfg:
+                self.app_cfg =cfg["application"]
+        
+       # print(cfg)
 
-        if "viaa" in cfg:
-            self.config = cfg["viaa"]
 
-    def get_config(self) -> dict:
-        return self.config
+
--- viaa/observability/logging.py
+++ viaa/observability/logging.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  @Author: Rudolf De Geijter
@@ -8,17 +9,17 @@
 import inspect
 import logging
 import sys
-
+import logging.config
 import structlog
 from pythonjsonlogger import jsonlogger
 from structlog._frames import _find_first_app_frame_and_name
 
 from viaa.configuration import ConfigParser
 
+
 loggers: dict = {}
 
-
-def get_logger(name="", config: ConfigParser = None):
+def get_logger(name="", config: ConfigParser=None):
     """Return a logger with the specified name and configuration, creating it if necessary.
     If no name is specified, return the root logger.
     If a config is specified it will override the current config for a logger.
@@ -34,11 +35,9 @@ def get_logger(name="", config: ConfigParser = None):
 
     if config is not None:
         logger = __configure(logger, config.config["logging"])
-
     return logger
 
-
-def __configure(logger, config: dict) -> object:
+def __configure(logger, config: dict) -> None:
     """Configures the logger with all relevant configuration from the passed config.
 
     Arguments:
@@ -52,6 +51,64 @@ def __configure(logger, config: dict) -> object:
 
 
 def __init():
+    import structlog
+    
+    timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
+    pre_chain = [
+        # Add the log level and a timestamp to the event_dict if the log entry
+        # is not from structlog.
+        structlog.stdlib.add_log_level,
+        __add_log_source_to_dict,
+        timestamper,
+ 
+    ]
+    config = ConfigParser().config
+    logging.config.dictConfig({
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "plain": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(colors=False),
+                    "foreign_pre_chain": pre_chain,
+                },
+                "colored": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(colors=True),
+                    "foreign_pre_chain": pre_chain,
+                },
+            },
+            "handlers": {
+                "default": {
+                    "level": config['logging']['level'],
+                    "class": "logging.StreamHandler",
+                    "formatter": "colored",
+                },
+                "file": {
+                    "level": config['logging']['level'],
+                    "class": "logging.handlers.WatchedFileHandler",
+                    "filename": __name__ + ".log",
+                    "formatter": "plain",
+                },
+                "rabbit":{
+                        "level": config['logging']['level'],
+                    "class": "python_logging_rabbitmq.RabbitMQHandlerOneWay",
+                    "host": config['logging']['RabPub']['host'],
+                    "username": config['logging']['RabPub']['user'],
+                    "password": config['logging']['RabPub']['passw'],
+                    "fields_under_root": True
+                    }
+                    
+            },
+            "loggers": {
+                "": {
+                    "handlers": ["default", "rabbit"],
+                    "level": config['logging']['level'],
+                    "propagate": True,
+                }                
+                               
+            }
+    })
     structlog.configure(
         processors=[
             # This performs the initial filtering, so we don't
@@ -71,7 +128,9 @@ def __init():
             # Decodes the unicode values in any kv pairs
             structlog.processors.UnicodeDecoder(),
             # Adds timestamp in iso format to each log
-            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.TimeStamper(
+                fmt="iso"
+            ),
             # Adds linenumber and file to each log
             __add_log_source_to_dict,
             structlog.stdlib.render_to_log_kwargs,
@@ -81,34 +140,56 @@ def __init():
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
-
+    
+        
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(jsonlogger.JsonFormatter())
     root_logger = logging.getLogger()
+    if len(root_logger.handlers) == 0:
+        root_logger.addHandler(handler)
 
-    # Handlers are removed because otherwise multiple get_logger calls leads to multiple log lines.
-    root_logger.handlers = []
-    root_logger.addHandler(handler)
 
 
 def __add_log_source_to_dict(logger, _, event_dict):
+
+    try:
+        from celery._state import get_current_task
+        get_current_task = get_current_task
+    except ImportError:
+        get_current_task = lambda: None
+    task = get_current_task()
+    if task and task.request and 'fields' in event_dict:
+        # if event_dict['fields'] is None:
+        #     event_dict['fields']={}
+        event_dict['fields']['celery_task_id'] = task.request.id
+        event_dict['fields']['celery_task_name'] = task.name
+        # event_dict.update(fields.task_id=task.request.id,
+        #                   fields.task_name=task.name)
+    # else:
+    #      event_dict.setdefault('fields', {})
+    #      event_dict.setdefault('fields', {})      
+
+        
     # If by any chance the record already contains a `source` key,
     # (very rare) move that into a 'source_original' key
-    if "source" in event_dict:
-        event_dict["source_original"] = event_dict["source"]
+    if 'source' in event_dict:
+        event_dict['source_original'] = event_dict['source']
 
-    (frame, name) = _find_first_app_frame_and_name(
-        additional_ignores=["logging", "correlation", "viaa", __name__]
-    )
+    f, name = _find_first_app_frame_and_name(additional_ignores=[
+        "logging",
+        __name__
+    ])
 
-    if not frame:
+    if not f:
         return event_dict
-    filename = inspect.getfile(frame)
-    frameinfo = inspect.getframeinfo(frame)
+    filename = inspect.getfile(f)
+    frameinfo = inspect.getframeinfo(f)
     if not frameinfo:
         return event_dict
     if frameinfo:
-        event_dict["source"] = "{}:{}:{}".format(
-            filename, frameinfo.function, frameinfo.lineno,
+        event_dict['source'] = '{}:{}:{}'.format(
+            filename,
+            frameinfo.function,
+            frameinfo.lineno,
         )
     return event_dict
